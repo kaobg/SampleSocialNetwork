a0b64885da3f7a9f00039fef8c78d003aa3a44d1
Калоян <kalala@mail.bg>
2013-11-13 20:42:03 +0200

    minor fix

--- SampleSocialNetwork/SampleSocialNetwork.Web/Hubs/MessagesHub.cs
+++ SampleSocialNetwork/SampleSocialNetwork.Web/Hubs/MessagesHub.cs
@@ -9,8 +9,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
-using System.Web.Helpers;
-using System.Web.Security;
 
 namespace SampleSocialNetwork.Hubs
 {
